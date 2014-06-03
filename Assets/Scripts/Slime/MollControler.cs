af0d06aae0432c66972080b41b703fb5135a7061
Nolimet <nolimet@stamdata.nl>
2014-06-03 13:48:22 +0200

    made gameplay working :P

---
+++ Assets/Scripts/Slime/MollControler.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MollControler : MonoBehaviour {
+
+    [SerializeField]
+    private SlimeBaseMove[] Slimes;
+
+    public int maxActivesSlimes;
+    [SerializeField]
+    public int currentActive;
+
+    private int startinlvl;
+
+    void Start()
+    {
+        Slimes = GetComponentsInChildren<SlimeBaseMove>();
+        for (int i = 0; i < 7; i++)
+        {
+            Slimes[i].ParentScrip = this;
+        }
+        startinlvl=Application.loadedLevel;
+        StartCoroutine("loader");
+    }
+    
+    IEnumerator loader()
+    {
+        while (startinlvl == Application.loadedLevel)
+        {
+            for (int i = 0; i < 7; i++)
+            {
+                float val = Random.Range(0, 10f);
+                // Debug.Log(i+" : "+val);
+                if (!Slimes[i].GotSlime && currentActive < maxActivesSlimes && val < 0.8f)
+                {
+                    Slimes[i].Slime = (SlimeBaseMove.SlimeType)(Mathf.FloorToInt(Random.Range(0, 8)));
+                    Slimes[i].GetSlime();
+                    currentActive++;
+                    yield return new WaitForSeconds(0.2f);
+                }
+            }
+            yield return new WaitForSeconds(0.1f);
+        }
+    }
+}
--- Assets/Scripts/Slime/SlimeBaseMove.cs
+++ Assets/Scripts/Slime/SlimeBaseMove.cs
@@ -2,99 +2,166 @@
 using System.Collections;
 
 [System.Serializable]
-[RequireComponent(typeof(BoxCollider2D))]
-public class SlimeBaseMove : MonoBehaviour {
-
-
+[RequireComponent(typeof(CircleCollider2D))]
+public class SlimeBaseMove : MonoBehaviour
+{
+#region vars
     private Animator amiTor;
-    private GameObject Go;
+    public MollControler ParentScrip;
+    [SerializeField]
+    private bool _GotSlime = false;
+    private bool SlimeDead = true;
+    public bool GotSlime
+    {
+        get
+        {
+            return _GotSlime;
+        }
+    }
 
     public enum SlimeType
     {
-        Bomb=0,
-        Boss=1,
-        Heart=2,
-        Helmet=3,
-        Ice=4,
-        Pinata=5,
-        Time=6,
-        Normal=7
+        Bomb = 0,
+        Boss = 1,
+        Heart = 2,
+        Helmet = 3,
+        Ice = 4,
+        Pinata = 5,
+        Time = 6,
+        Normal = 7
     }
 
-    [SerializeField]
-    private SlimeType Slime;
+    public SlimeType Slime;
 
     [SerializeField]
     private int HP;
-    
-    void Start()
+
+    private float WaitTime = 0f; //used so animations dont cross paths
+    private float ToSlow; // when zero the slime dies
+#endregion
+    public void GetSlime()
     {
-        
-        string toLoad ="Slimes/";
+        string toLoad = "Slimes/";
+        int numbOfFrames = 1; // frames in death animation;
+
+        ToSlow = 2f;
+
         switch (Slime)
         {
             case SlimeType.Bomb:
                 HP = 1;
                 toLoad += "Bomb";
+                numbOfFrames = 9;
                 break;
             case SlimeType.Boss:
                 HP = 1;
                 toLoad += "Boss";
+                numbOfFrames = 9;
                 break;
             case SlimeType.Heart:
                 HP = 1;
                 toLoad += "Heart";
+                numbOfFrames = 9;
                 break;
             case SlimeType.Helmet:
-                HP = 1;
+                HP = 3;
                 toLoad += "Helmet";
+                numbOfFrames = 9;
+                ToSlow = 4f;
                 break;
             case SlimeType.Ice:
                 HP = 1;
                 toLoad += "Ice";
+                numbOfFrames = 9;
                 break;
             case SlimeType.Pinata:
                 HP = 1;
                 toLoad += "Pinata";
+                numbOfFrames = 9;
                 break;
             case SlimeType.Time:
                 HP = 1;
                 toLoad += "Time";
+                numbOfFrames = 9;
                 break;
             case SlimeType.Normal:
-                toLoad+="Normal";
+                HP = 1;
+                toLoad += "Normal";
+                numbOfFrames = 9;
+                break;
+
+            default:
+                 HP = 1;
+                toLoad += "Normal";
+                numbOfFrames = 9;
                 break;
         }
+        _GotSlime = true;
+        SlimeDead = false;
 
-        
-        Go = Instantiate(Resources.Load(toLoad)) as GameObject;
+        WaitTime = (1f / 23 * numbOfFrames);
+
+        GameObject Go = Instantiate(Resources.Load(toLoad)) as GameObject;
         Go.transform.parent = transform;
         Go.transform.localPosition = Vector3.zero;
 
         amiTor = Go.GetComponent<Animator>();
         Debug.Log(amiTor);
     }
-    
+
     //activated by hitRayFirere
     public void hit()
     {
-        if (HP > 1)
+        if (_GotSlime && amiTor != null)
         {
-            amiTor.SetTrigger("GotHit");
-            
-            HP--;
-        }
-        else
-        {
-            amiTor.SetTrigger("FinalHit");
-            Destroy(amiTor.gameObject, 1f);
-            HP--;
-            died();
+            if (HP > 1)
+            {
+                amiTor.SetTrigger("GotHit");
+                HP--;
+            }
+            else
+            {
+                killAction();
+                //addScore;
+            }
         }
     }
 
-    void died()
+    void Update()
     {
-        Destroy(Go, 1f);
+        if (SlimeDead&&_GotSlime)
+        {
+            if (WaitTime < 0)
+            {
+                SlimeDead = true;
+                _GotSlime = false;
+                Debug.Log("deadSlime");
+            }
+            else
+            {
+                WaitTime -= Time.deltaTime;
+            }
+        }
+        else if (!SlimeDead && _GotSlime)
+        {
+            if (ToSlow <= 0)
+            {
+                killAction();
+            }
+            else
+            {
+                ToSlow -= Time.deltaTime;
+            }
+        }
+    }
+
+    void killAction()
+    {
+        Destroy(amiTor.gameObject, 1f);
+        amiTor.SetTrigger("FinalHit");
+        Destroy(amiTor.gameObject, 1f);
+        SlimeDead = true;
+        HP = 0;
+        ParentScrip.currentActive--;
     }
 }
