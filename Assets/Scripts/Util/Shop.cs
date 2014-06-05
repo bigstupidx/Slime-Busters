361dbedcf783772aede25c30d165d9af41be6bb7
Kit van de Bunt <kitlvdbunt@hotmail.com>
2014-06-05 17:08:09 +0200

    Shop added

---
+++ Assets/Scripts/Util/Shop.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shop : MonoBehaviour {
+
+    [SerializeField]
+    private UILabel CostLable;
+
+	void Start () {
+        CostLable.text = "Cost:500";
+	}
+
+    public void Back()
+    {
+        Application.LoadLevel("MainMenu");
+    }
+
+    public void Buy()
+    {
+        Debug.Log("Buy");
+    }
+
+    public void Use()
+    {
+        Debug.Log("Use");
+    }
+}
