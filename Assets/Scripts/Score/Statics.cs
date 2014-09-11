8506cd9c42a202fcb9e5ae3341ff536c03d6010c
Nolimet <nolimet@stamdata.nl>
2014-09-11 15:01:14 +0200

    added some score related systems
    A static var in Score.Statics named score& highScore
    Also added a score manager that handels a dubble score timer

---
+++ Assets/Scripts/Score/Statics.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace Score
+{
+    public class Statics
+    {
+
+        public static int Score = 0;
+        public static int HighScore = 0;
+
+        public static int chain = 0;
+        public static bool Bonus;
+        public static bool bonusActivated;
+
+        public static void AddScore(int amount)
+        {
+            if (!Bonus)
+                Score += amount;
+            else
+                Score += amount * 2;
+
+            if (Score > HighScore)
+                HighScore = Score;
+        }
+
+        public static void xpbonus()
+        {
+            if(!Bonus)
+                 bonusActivated = true;
+        }
+    }
+}
