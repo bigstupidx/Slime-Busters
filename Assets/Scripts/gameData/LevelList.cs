b4fdc7998b721e871dfba5212281f124168349fa
Kit van de Bunt <kitlvdbunt@hotmail.com>
2014-11-02 16:03:52 +0100

    levels

---
+++ Assets/Scripts/gameData/LevelList.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelList : MonoBehaviour {
+    [SerializeField]
+    private GameObject[] levels;
+
+    private void Start(){
+        GameObject.Instantiate(levels[(int)Random.Range(0,levels.Length)],Vector3.zero,Quaternion.identity);
+    }
+}
